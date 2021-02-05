2124ab814305aac3db66dfcf03fe07acb7261c2b
saikumargit-hub <azure.saikumar@gmail.com>
2021-02-05 09:35:14 +0000

    SAP UI5 Demo Application in BAS

---
+++ HTML5Module/webapp/controller/Main.controller.js
@@ -0,0 +1,19 @@
+sap.ui.define([
+     "sap/ui/core/mvc/Controller",
+     "sap/ui/model/json/JSONModel"
+],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+     function (Controller, jsonmodel) {
+          "use strict";
+
+          return Controller.extend("ns.HTML5Module.controller.Main", {
+               onInit: function () {
+                    this.LocalJSONModel = new jsonmodel({
+                         empTableModel: []
+                    });
+                    this.getView().setModel(this.LocalJSONModel, "localModel");
+               }
+          });
+     });
---
+++ HTML5Module/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"ns/HTML5Module/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
